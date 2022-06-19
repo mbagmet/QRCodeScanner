2e3d011a8b7629a20a6b1830471a9fa2842225cb
Bagmet <m.bagmet@gmail.com>
2022-06-19 16:06:54 +0200

    Add web page loading progressBar

--- QRCodeScanner/Sources/Scenes/ScanResults/View/ScanResultsViewController.swift
+++ QRCodeScanner/Sources/Scenes/ScanResults/View/ScanResultsViewController.swift
@@ -14,6 +14,15 @@ class ScanResultsViewController: UIViewController, WKUIDelegate {
     
     var presenter = ScanResultsPresenter()
     
+    // MARK: - Views
+    
+    private lazy var progressView: UIProgressView = {
+        let progressView = UIProgressView(progressViewStyle: .bar)
+        progressView.sizeToFit()
+        
+        return progressView
+    }()
+    
     // MARK: - Lifecycle
     
     var webView: WKWebView!
@@ -22,6 +31,7 @@ class ScanResultsViewController: UIViewController, WKUIDelegate {
         let webConfiguration = WKWebViewConfiguration()
         webView = WKWebView(frame: .zero, configuration: webConfiguration)
         webView.uiDelegate = self
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
         view = webView
     }
     
@@ -37,6 +47,48 @@ class ScanResultsViewController: UIViewController, WKUIDelegate {
         // MARK: Presenter setup
         presenter.setViewDelegate(delegate: self)
         presenter.getUrl()
+        
+        // MARK: View setup
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        progressView.removeFromSuperview()
+        
+        super.viewWillDisappear(animated)
+    }
+    
+    // MARK: - Settings
+    
+    private func setupHierarchy() {
+        navigationController?.navigationBar.addSubview(progressView)
+    }
+    
+    private func setupLayout() {
+        progressView.snp.makeConstraints { make in
+            make.bottom.equalToSuperview()
+            make.width.equalToSuperview()
+        }
+    }
+    
+    // MARK: - Methods
+    
+    // MARK: Oserve webView loading progress
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+            
+            if webView.estimatedProgress == 1.0 {
+                progressView.removeFromSuperview()
+            }
+        }
+    }
+    
+    // MARK: - Private functions
+    
+    private func addObserver() {
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
     }
 }
 
