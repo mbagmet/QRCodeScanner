d232352b390f9e31d1cd9b4cd7100b4260bb427f
Bagmet <m.bagmet@gmail.com>
2022-06-22 21:56:41 +0200

    Delete unused url

--- QRCodeScanner/Sources/Scenes/ScanResults/Presenter/ScanResultsPresenter.swift
+++ QRCodeScanner/Sources/Scenes/ScanResults/Presenter/ScanResultsPresenter.swift
@@ -13,7 +13,6 @@ class ScanResultsPresenter: ScanResults {
     
     weak var view: ScanResultsPresenterDelegate?
     var url: URL?
-    //var url = URL(string: "https://unec.edu.az/application/uploads/2014/12/pdf-sample.pdf")
     
     private var mimeTypes = [MimeType(type: "pdf", fileExtension: "pdf")]
     private var fileDownloadProvider: FileDownloadProvider?
